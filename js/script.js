86f913f1c9d2b8c79895eac2ebf7dc4369cc290b
Tomasz Rojek <tomekr@gmail.com>
2019-05-22 13:48:25 +0200

    Add  'active' method

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 'use strict';
 function titleClickHandler(event){
-    console.log(event);
-  
-    /* remove class 'active' from all article links  */
+    const clickedElement = this;
+    console.log('clickedElement (with plus): ' + clickedElement);
+    clickedElement.classList.add('active');
+    console.log('activeElement (with plus): ' + clickedElement);
+    event.preventDefault();
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
      for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
      }
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+     
+        
+    /* [DONE] remove class 'active' from all articles */
     
     const activeArticles = document.querySelectorAll('.active');
 
