cfd71731a445be2c72241aac6926645ee1dcd32a
Tomasz Rojek <tomekr@gmail.com>
2019-05-22 19:36:39 +0200

    Done first part of projec

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 'use strict';
 function titleClickHandler(event){
     const clickedElement = this;
+     
     console.log('clickedElement (with plus): ' + clickedElement);
-    clickedElement.classList.add('active');
-    console.log('activeElement (with plus): ' + clickedElement);
+    
+    
     event.preventDefault();
+
+
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
      for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
      }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
-     
+    /* [DONE] add class 'active' to the clicked link */
+        clickedElement.classList.add('active');
         
     /* [DONE] remove class 'active' from all articles */
     
@@ -23,11 +26,18 @@ function titleClickHandler(event){
     }
 
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(' href atribute (with plus): ' + articleSelector);
   
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
+    /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
+        const  targetArticle = document.querySelector(articleSelector);
+        console.log('find correct article (with plus): ' + targetArticle);
+
     /* add class 'active' to the correct article */
+   
+    targetArticle.classList.add('active');
     }
   
   const links = document.querySelectorAll('.titles a');
