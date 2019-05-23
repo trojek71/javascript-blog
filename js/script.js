f1a05e5992340f8d4a4648f50ffccd526d539ad4
Tomasz Rojek <tomekr@gmail.com>
2019-05-23 11:01:39 +0200

    Add function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -56,24 +56,33 @@ function titleClickHandler(event){
 
   function generateTitleLinks(){
 
-    /* [IN PROGRESS] remove contents of titleList */
+    /* [DONE] remove contents of titleList */
      const titleList = document.querySelector(optTitleListSelector);
-     console.log('find correct article (with plus): ' , titleList);
+     console.log('remove contents of titleList: ' , titleList);
      titleList.innerHTML = '';
     
 
-     /* for each article */
-  
+     /* [IN PROGRESS] for each article */
+      const articles = document.querySelectorAll(optArticleSelector);
+      console.log('for each article: ' , articles);
+      
+      for(let article of articles){
+       document.querySelector(optArticleSelector);
+       
       /* get the article id */
-  
-      /* find the title element */
-  
+      const articleId = article.getAttribute('id');
+       console.log('get the article id: ' , articleId);
+       
+       /* find the title element */
+       const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+       console.log('Article title  ' , articleTitle);
       /* get the title from the title element */
-  
+      
       /* create HTML of the link */
   
       /* insert link into titleList */
   
+      }   
   }
   
   generateTitleLinks();
